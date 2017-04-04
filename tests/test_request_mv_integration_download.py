a20c565486e5060945192b4ac54ff010334ab692
avital <avital@tune.com>
2017-04-04 12:25:29 +0300

    [MVI-1154] uncomment and refactor

--- tests/test_request_mv_integration_download.py
+++ tests/test_request_mv_integration_download.py
@@ -5,20 +5,17 @@
 import pytest
 import os
 import tempfile
-
-tmpdir = tempfile.mkdtemp()
-
+import csv
+from os import sep
+from subprocess import Popen, PIPE
 from .resources.mockserver import (run_server, HTTP_SERVER_PORT, StaticFilesHandler)
 from requests_mv_integrations import (
     RequestMvIntegrationDownload,
 )
 from requests_mv_integrations.exceptions.custom import (
     TuneRequestModuleError,
-    TuneRequestServiceError
 )
-import csv
-from os import sep
-from subprocess import Popen, PIPE
+tmpdir = tempfile.mkdtemp()
 
 __all__ = [run_server]
 
@@ -95,26 +92,26 @@ def compare_csv_files(file_path_1, file_path_2):
 
 
 class TestRequestMvIntegrationDownload:
-    # def test_request_csv_download(self, request_mv_integration_download_object, run_server):
-    #     # RequestMvIntegrationDownload.request_csv_download(...) returns a
-    #     # generator containing CSV data by rows in JSON dictionary format.
-    #     # Converting the generator to a list, causes yielding all rows values.
-    #     csv_as_list = list(
-    #         request_mv_integration_download_object.request_csv_download(
-    #             request_method='GET',
-    #             request_url="test_csv_download_url",
-    #             tmp_csv_file_name=TMP_CSV_FILE_NAME,
-    #             tmp_directory=tmpdir,
-    #         )
-    #     )
-    #     # as a side effect, the method saves a csv file in <TMP_DIRECTORY>/<TMP_CSV_FILE_NAME>.
-    #     # Check, whether the method has yielded the CSV file values correctly ( We mocked an
-    #     # HTTP GET server to return in the response, the content of a predefined csv file )
-    #     downloaded_file_path = tmpdir + sep + TMP_CSV_FILE_NAME
-    #     assert (compare_csv_files(StaticFilesHandler.csv_file_name(), downloaded_file_path))
-    #
-    #     # Now, check that the content of the list of csv dictionary rows, is correct
-    #     assert (compare_csv_file_to_csv_list(StaticFilesHandler.csv_file_name(), csv_as_list))
+    def test_request_csv_download(self, request_mv_integration_download_object, run_server):
+        # RequestMvIntegrationDownload.request_csv_download(...) returns a
+        # generator containing CSV data by rows in JSON dictionary format.
+        # Converting the generator to a list, causes yielding all rows values.
+        csv_as_list = list(
+            request_mv_integration_download_object.request_csv_download(
+                request_method='GET',
+                request_url="test_csv_download_url",
+                tmp_csv_file_name=TMP_CSV_FILE_NAME,
+                tmp_directory=tmpdir,
+            )
+        )
+        # as a side effect, the method saves a csv file in <TMP_DIRECTORY>/<TMP_CSV_FILE_NAME>.
+        # Check, whether the method has yielded the CSV file values correctly ( We mocked an
+        # HTTP GET server to return in the response, the content of a predefined csv file )
+        downloaded_file_path = tmpdir + sep + TMP_CSV_FILE_NAME
+        assert (compare_csv_files(StaticFilesHandler.csv_file_name(), downloaded_file_path))
+
+        # Now, check that the content of the list of csv dictionary rows, is correct
+        assert (compare_csv_file_to_csv_list(StaticFilesHandler.csv_file_name(), csv_as_list))
 
     def test_request_csv_download_fail(self, request_mv_integration_download_object, run_server):
         # RequestMvIntegrationDownload.request_csv_download(...) returns a
@@ -129,47 +126,47 @@ class TestRequestMvIntegrationDownload:
                     tmp_directory=tmpdir,
                 )
             )
-            assert "Invalid URL" in str(info.value)
+            assert "Invalid URLp" in str(info.value)
 
-    # def test_request_json_download(self, request_mv_integration_download_object, run_server):
-    #     # RequestMvIntegrationDownload.request_json_download(...) should save the content of a response to
-    #     # a GET request to <test_json_download_url>, as a json file at <TMP_DIRECTORY>/<TMP_JSON_FILE_NAME>
-    #     request_mv_integration_download_object.request_json_download(
-    #         request_method='GET',
-    #         request_url=test_json_download_url,
-    #         tmp_json_file_name=TMP_JSON_FILE_NAME,
-    #         tmp_directory=tmpdir,
-    #     )
-    #
-    #     # Check, whether the method has saved the file correctly ( We mocked the an HTTP GET server to return a
-    #     # predefined json file )
-    #     downloaded_file_path = tmpdir + sep + TMP_JSON_FILE_NAME
-    #     mock_file_path = StaticFilesHandler.json_file_name()
-    #
-    #     # Compare two files by running a diff command in shell
-    #     sts = Popen(['diff', downloaded_file_path, mock_file_path], stdout=PIPE, stderr=PIPE, stdin=PIPE)
-    #     diff_res = sts.stdout.read()
-    #     assert (diff_res == b'')
-    #
-    # def test_stream_csv(self, request_mv_integration_download_object, run_server):
-    #     # RequestMvIntegrationDownload.stream_csv(...) return a generator.
-    #     # Converting the generator to a list, causes yielding all values.
-    #     csv_as_list = list(
-    #         request_mv_integration_download_object.stream_csv(
-    #             request_url=test_csv_stream_url,
-    #             request_params=None,
-    #         )
-    #     )
-    #
-    #     # Check, wether the method has streamed the CSV file correctly ( We mocked the an HTTP GET server to
-    #     # return in the response, the content of a predefined csv file )
-    #     assert (compare_csv_file_to_csv_list(StaticFilesHandler.csv_file_name(), csv_as_list))
-    #
-    # def test_session(self, request_mv_integration_download_object, run_server):
-    #     assert request_mv_integration_download_object.session is not None
-    #
-    # def test_tune_request(self, request_mv_integration_download_object, run_server):
-    #     assert request_mv_integration_download_object.tune_request is not None
-    #
-    # def test_mv_request(self, request_mv_integration_download_object, run_server):
-    #     assert request_mv_integration_download_object.mv_request is not None
+    def test_request_json_download(self, request_mv_integration_download_object, run_server):
+        # RequestMvIntegrationDownload.request_json_download(...) should save the content of a response to
+        # a GET request to <test_json_download_url>, as a json file at <TMP_DIRECTORY>/<TMP_JSON_FILE_NAME>
+        request_mv_integration_download_object.request_json_download(
+            request_method='GET',
+            request_url=test_json_download_url,
+            tmp_json_file_name=TMP_JSON_FILE_NAME,
+            tmp_directory=tmpdir,
+        )
+
+        # Check, whether the method has saved the file correctly ( We mocked the an HTTP GET server to return a
+        # predefined json file )
+        downloaded_file_path = tmpdir + sep + TMP_JSON_FILE_NAME
+        mock_file_path = StaticFilesHandler.json_file_name()
+
+        # Compare two files by running a diff command in shell
+        sts = Popen(['diff', downloaded_file_path, mock_file_path], stdout=PIPE, stderr=PIPE, stdin=PIPE)
+        diff_res = sts.stdout.read()
+        assert (diff_res == b'')
+
+    def test_stream_csv(self, request_mv_integration_download_object, run_server):
+        # RequestMvIntegrationDownload.stream_csv(...) return a generator.
+        # Converting the generator to a list, causes yielding all values.
+        csv_as_list = list(
+            request_mv_integration_download_object.stream_csv(
+                request_url=test_csv_stream_url,
+                request_params=None,
+            )
+        )
+
+        # Check, wether the method has streamed the CSV file correctly ( We mocked the an HTTP GET server to
+        # return in the response, the content of a predefined csv file )
+        assert (compare_csv_file_to_csv_list(StaticFilesHandler.csv_file_name(), csv_as_list))
+
+    def test_session(self, request_mv_integration_download_object, run_server):
+        assert request_mv_integration_download_object.session is not None
+
+    def test_tune_request(self, request_mv_integration_download_object, run_server):
+        assert request_mv_integration_download_object.tune_request is not None
+
+    def test_mv_request(self, request_mv_integration_download_object, run_server):
+        assert request_mv_integration_download_object.mv_request is not None
